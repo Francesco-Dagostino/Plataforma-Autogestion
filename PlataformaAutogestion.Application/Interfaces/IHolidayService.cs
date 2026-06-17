aaeabf0781c3c42b1c267ac7134b982b3c37221c
danilomercado <mercadodanilo213@hotmail.com>
2026-06-17 16:14:35 -0300

    Integracion de API Externa de feriados

---
+++ PlataformaAutogestion.Application/Interfaces/IHolidayService.cs
@@ -0,0 +1,10 @@
+﻿using PlataformaAutogestion.Application.DTOs;
+
+namespace PlataformaAutogestion.Application.Interfaces
+{
+    public interface IHolidayService
+    {
+        Task<IEnumerable<HolidayDTO>> GetHolidaysByYearAsync(int year);
+        Task<bool> IsHolidayAsync(DateTime date);
+    }
+}
