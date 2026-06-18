8de79d8ef277548e6b5a396282e31b7721956c6e
danilomercado <mercadodanilo213@hotmail.com>
2026-06-18 15:29:17 -0300

    liquidation/endpoint

---
+++ PlataformaAutogestion.Application/Models/Response/DetalleLiquidacionResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlataformaAutogestion.Application.Models.Response
+{
+    public class DetalleLiquidacionResponse
+    {
+        public int IdUser { get; set; }
+        public string NombreEmpleado { get; set; }
+        public decimal TotalHoras { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
